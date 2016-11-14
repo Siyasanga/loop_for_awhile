02b55ed31a3138f0faab2867c600c138db769072
Siyasanga <siyasanga@live.co.za>
2016-11-14 15:11:13 +0200

    Using for loops to return all registrations from paarl, having problems comparing arrays through assert.equal though

---
+++ allPaarl.js
@@ -0,0 +1,17 @@
+// This function takes in regNumbers which is a string of comma seperated
+// registration numbers. It splits the string into an array
+// Using a forIn loop it probes the array to return all registrations starting with 'CJ'
+function allPaarl(regNumbers){
+  var regList = regNumbers.split(',');
+  var allFromPaarl = [];
+  for(i in regList){
+    if(regList[i].startsWith('CJ')){
+      allFromPaarl.push(regList[i]);
+    }
+  }
+  return allFromPaarl;
+}
+const assert = require('assert');
+allFromPaarl = allPaarl('CA 182736,CY 523519,CJ 812328,CJ 111222,EC 812328');
+console.log(allFromPaarl);
+assert.equal(allPaarl('CA 182736,CY 523519,CJ 812328,CJ 111222,EC 812328'),allFromPaarl);

0d25d054077115ee3037712adacb8e41d5034044
Siyasanga <siyasanga@live.co.za>
2016-11-14 12:17:54 +0200

    Working with the for loop

---
+++ e-fruits.js
@@ -0,0 +1,14 @@
+var fruits = ['apple', 'pear', 'mango', 'orange', 'banana'];
+
+// create an empty list for fruits ending with 'e'
+var efruits = [];
+for (var i=0;i<fruits.length;i++){
+    var fruit = fruits[i];
+    //check if the current fruit end with 'e'
+    if (fruit.endsWith('e')){
+        // add fruit ending with 'e' to the list
+        efruits.push(fruit);
+    }
+}
+// should print ['apple', 'orange']
+console.log(efruits);

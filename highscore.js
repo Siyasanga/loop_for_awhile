fb6d073b1220088c531425e9f0d118f733a9c481
Siyasanga <siyasanga@live.co.za>
2016-11-14 12:48:05 +0200

    Running the loops examples

---
+++ highscore.js
@@ -0,0 +1,12 @@
+var scores = [56,73,51,87,45,23,47];
+var highScore = 0;
+
+for(var i=0; i<scores.length; i++;){
+    var score = scores[i];
+    if (score > highScore){
+        highScore = score;
+    }
+}
+
+//this should print 87
+console.log(highScore);
